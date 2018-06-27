73f04ea74fd7b0530879b3e1d10ed8cfe5d7dbaa
Sean Helm <shelm@gmu.edu>
2018-06-26 23:30:09 -0400

    Added MongoDB support

--- streaming/reddit.py
+++ streaming/reddit.py
@@ -1,7 +1,10 @@
-import praw
 from bs4 import BeautifulSoup
-from markdown import markdown
 from configparser import ConfigParser
+from datetime import datetime
+from markdown import markdown
+from praw import Reddit
+from pymongo import MongoClient
+from textblob import TextBlob
 
 
 def remove_markdown(text):
@@ -10,14 +13,15 @@ def remove_markdown(text):
 
     Args:
         text: String of raw markdown text
-    
+
     Returns:
         A processed string without any markdown syntax
     """
     html = markdown(text)
-    return ''.join(BeautifulSoup(html).findAll(text=True))
+    return ''.join(BeautifulSoup(html, "html.parser").findAll(text=True))
 
-def process_comment(comment):
+
+def process_comment(comment, collection, max_polarity):
     """
     Process the entire comment by removing markdown syntax and performing sentiment
     analysis on the comment body.
@@ -26,21 +30,44 @@ def process_comment(comment):
         comment: Entire comment information
     """
     body_plain = remove_markdown(comment.body)
-    print("'%s'\n" % (body_plain.replace('\n', ' ')))
+    blob = TextBlob(body_plain)
+
+    # If comment is suspicious
+    if blob.sentiment[0] <= -0.2:
+        # Extract necessary information from reddit comment
+        other_keys = ['permalink', 'subreddit', 'link_title']
+        comment_dict = {key: comment.__dict__[key] for key in other_keys}
+        comment_dict.update(author=comment.author.name,
+                            subreddit=comment.author.name,
+                            created=datetime.utcfromtimestamp(comment.created_utc),
+                            body=body_plain,
+                            polarity=blob.sentiment[0])
+
+        # Insert into database
+        id = collection.insert_one(comment_dict).inserted_id
+        print('\t%s: %s' % (id, comment_dict))
 
 
 if __name__ == '__main__':
     config = ConfigParser()
     config.read('config.cfg')
 
-    # Generate PRAW instance
-    reddit = praw.Reddit(client_id=config['CREDENTIALS']['ClientID'],
-        client_secret=config['CREDENTIALS']['ClientSecret'],
-        user_agent=config['CREDENTIALS']['UserAgent'],
-        username=config['CREDENTIALS']['Username'],
-        password=config['CREDENTIALS']['Password'])
+    # Connect to MongoDB
+    client = MongoClient(config['MONGODB']['Host'])
+    db = client.streddit
+    coll = db.comments
 
-    # Stream comments on r/politics
+    # Generate PRAW instance
+    reddit = Reddit(client_id=config['CREDENTIALS']['ClientID'],
+                    client_secret=config['CREDENTIALS']['ClientSecret'],
+                    user_agent=config['CREDENTIALS']['UserAgent'],
+                    username=config['CREDENTIALS']['Username'],
+                    password=config['CREDENTIALS']['Password'])
+
+    # Consume comments stream
     subreddit = reddit.subreddit(config['REDDIT']['Subreddit'])
+    print("Consuming Reddit stream from r/%s..." % subreddit)
+
+    # S
     for comment in subreddit.stream.comments():
-        process_comment(comment)
+        process_comment(comment, coll, config['REDDIT']['MaxPolarity'])
