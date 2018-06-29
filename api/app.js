a80d44f1a7b5914ca0b475d086e721f270239529
Sean Helm <shelm@gmu.edu>
2018-06-29 18:04:59 -0400

    Added functionality to view reddit comments through client app

--- api/app.js
+++ api/app.js
@@ -3,10 +3,14 @@ var app = express()
 var mongoose = require('mongoose')
 var Comment = require('./models/commentsModel')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 
 // Connect to MongoDB
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/streddit')
+
+// Enable CORS
+app.use(cors())
 
 // Use BodyParser for POST
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,4 +20,6 @@ app.use(bodyParser.json())
 var comments = require('./routes/commentsRouter')
 app.use('/comments', comments)
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("API listening on port 3000")
+})
--- client/src/components/ListComments.vue
+++ client/src/components/ListComments.vue
@@ -1,27 +1,31 @@
 <template>
     <div id="comments">
-        <li v-for="comment in comments">
-            {{ comment.body }}, {{ comment.author }}
-        </li>
+        <h3 class="title is-3">Review negative Reddit comments</h3>
+        <article class="message" v-for="comment in comments">
+            <div class="message-header">
+               <p>{{ comment.author }}</p>
+               <button class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+                {{ comment.body }}
+            </div>
+        </article>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'ListComments',
     data () {
         return {
-            comments: [
-                {
-                    body: "Henlo lizer",
-                    author: "James123"
-                },
-                {
-                    body: "How are you today im fine thank you",
-                    author: "23aas43"
-                }
-            ]
+            comments: null
         }
+    },
+    mounted () {
+        axios.get("http://localhost:3000/comments/")
+        .then(response => (this.comments = response.data))
     }
 }
 </script>
