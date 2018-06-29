8aef9660434a8546f4d943c454eeddf7a0429732
Sean Helm <shelm@gmu.edu>
2018-06-29 16:17:45 -0400

    Added basic list rendering component

---
+++ client/src/components/ListComments.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="comments">
+        <li v-for="comment in comments">
+            {{ comment.body }}, {{ comment.author }}
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ListComments',
+    data () {
+        return {
+            comments: [
+                {
+                    body: "Henlo lizer",
+                    author: "James123"
+                },
+                {
+                    body: "How are you today im fine thank you",
+                    author: "23aas43"
+                }
+            ]
+        }
+    }
+}
+</script>
